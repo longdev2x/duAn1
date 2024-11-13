632547cacca66d36fa1d181d64344a72e57fefc5
LogiFlutter <hoangnhatlong17042000@gmail.com>
2024-11-13 10:11:41 +0700

    Update .gitignore

--- lib/data/repository/language_repo.dart
+++
@@ -1,9 +0,0 @@
-import 'package:du_an_1/data/model/response/language_model.dart';
-import 'package:flutter/material.dart';
-import '../../utils/app_constants.dart';
-
-class LanguageRepo {
-  List<LanguageModel> getAllLanguages({required BuildContext context}) {
-    return AppConstants.languages;
-  }
-}
