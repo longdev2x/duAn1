e6ba14b3be41cc69e7b1446a2d9d9d99a9ad18be
ducnaph53199 <ducnaph53199@gmail.com>
2024-11-15 08:36:44 +0700

    Đức:Code Ui màn hình notify

---
+++ lib/screens/notify/notify_screen.dart
@@ -0,0 +1,46 @@
+import 'package:du_an_1/screens/notify/widgets/notification_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:du_an_1/controller/notification_controller.dart';
+
+import 'package:du_an_1/view/app_text.dart';
+
+class NotifyScreen extends StatelessWidget {
+  const NotifyScreen({super.key});
+
+  void _testNotify() {
+    Get.find<NotificationController>().testPushNotify();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, 80.h),
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Row(
+              children: [
+                AppText24(
+                  'notification'.tr,
+                  fontWeight: FontWeight.bold,
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: _testNotify,
+                  child: const Text('Test Notify'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(16.w, 20.h, 16.w, 0),
+        child: const NotificationList(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/notify/widgets/notification_item.dart
@@ -0,0 +1,77 @@
+import 'package:du_an_1/data/model/response/notification_entity.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import 'package:du_an_1/helper/date_converter.dart';
+import 'package:du_an_1/utils/images.dart';
+import 'package:du_an_1/view/app_image.dart';
+import 'package:du_an_1/view/app_text.dart';
+
+class NotificationItem extends StatelessWidget {
+  final NotificationEntity objNotify;
+  const NotificationItem({super.key, required this.objNotify});
+
+  String _getDateFormat(DateTime? date) {
+    if (date == null) return 'no_data_found'.tr;
+    DateTime now = DateTime.now();
+
+    int difference = now.difference(date).inMinutes;
+
+    if (difference < 60) {
+      return '$difference ${'minutes_ago'.tr}';
+    }
+    if (difference < 1440) {
+      int hours = now.difference(date).inHours;
+      return '$hours ${'hours_ago'.tr}';
+    }
+    if (difference < 10080) {
+      int days = now.difference(date).inDays;
+      return '$days ${'days_ago'.tr}';
+    }
+    return '${DateConverter.getWeekDay(objNotify.date!)} - ${'days'} ${DateConverter.getOnlyFomatDate(objNotify.date!)}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 12.h,
+      ),
+      // decoration: BoxDecoration(
+      //   color: ColorResources.getBackgroundColor(),
+      // ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CircleAvatar(
+            backgroundImage: objNotify.user?.image != null
+                ? NetworkImage(objNotify.user!.image!)
+                : const AssetImage(Images.imgAvatarDefault) as ImageProvider,
+            radius: 25,
+          ),
+          SizedBox(width: 12.w),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AppText16(
+                objNotify.title,
+                fontWeight: FontWeight.bold,
+              ),
+              AppText14(objNotify.body),
+              AppText14(_getDateFormat(objNotify.date)),
+            ],
+          ),
+          const Spacer(),
+          AppImageAsset(
+            onTap: () {},
+            imagePath: Images.icThreeDotHori,
+            width: 15,
+            height: 3,
+          ),
+        ],
+      ),
+    );
+  }
+}
